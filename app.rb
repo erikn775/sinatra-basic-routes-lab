1f934c95dc6472bc6e4f2bf998b58061b88589f8
erikn775 <erikn775@gmail.com>
2020-11-23 10:49:59 -0700

    commit tree 29cb38056430f5dc612f10122314c13c9bee1d8c

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Erik"
+    end
+
+    get '/hometown' do
+        "My hometown is Colorado Springs"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Famous Last Words"
+    end
 end
